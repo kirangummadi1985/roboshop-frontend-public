4374ef873d3547656830e668c062099043b06de1
kiran <kirangummadi1985@gmail.com>
2022-06-27 18:58:10 +0530

    Infra-setup

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "alb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -55,7 +55,7 @@ resource "aws_security_group" "alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
